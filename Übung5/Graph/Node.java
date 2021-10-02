eff7edc8c3262a2932853f4496502b2fe4ea7d9e
Benjamin Karatschinez <b.karatschinez@tu-bs.de>
2021-10-02 12:19:58 +0200

    Edited Graph structure

---
+++ Übung5/Graph/Node.java
@@ -0,0 +1,23 @@
+package Graph;
+public class Node {
+    private String val;
+    
+    public Node(String val) {
+        this.val = val;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Node o = (Node) obj;
+        return this.val.equals(o.val);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.val.hashCode();
+    }
+
+    public String getVal() {
+        return val;
+    }
+}
