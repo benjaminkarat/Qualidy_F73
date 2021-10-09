90e92d06e2adb251c2c68696edd9b109988abefd
Benjamin Karatschinez <b.karatschinez@tu-bs.de>
2021-10-09 14:36:53 +0100

    Main zum Testen des Suchbaums

---
+++ Übung6/Suchbaum/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Suchbaum<Integer> sb = new Suchbaum<>();
+        //sb.add(1);
+        //sb.contains(1);
+        //sb.getMin();
+        //sb.getMax();
+    }
+}
