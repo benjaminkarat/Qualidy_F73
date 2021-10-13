6e8cbb7d856389356e671f2df72fa65993ac3c58
Benjamin Karatschinez <b.karatschinez@tu-bs.de>
2021-10-13 13:32:08 +0100

    Revert "Klassenkopf korrigiert"
    This reverts commit 79a807f47b05939ba0c52d1b66f78c1e1b3f63f3.

--- Übung6/Suchbaum/Suchbaum.java
+++ Übung6/Suchbaum/Suchbaum.java
@@ -1,6 +1,6 @@
 import java.util.Collection;
 
-public class Suchbaum<E extends Comparable> {
+public class Suchbaum<E> implements Comparable<E> {
     private E value;
     private Suchbaum<E> left;
     private Suchbaum<E> right;
@@ -11,57 +11,37 @@ public class Suchbaum<E extends Comparable> {
     }
 
     // Erstellen eines Baumes mit Werten aus einem Array
-    public Suchbaum(E[] arr, E val) {
-        this.value = val;
-        for (E e : arr) {
-            this.add(e);
-        }
+    public Suchbaum(E[] arr) {
+
     }
 
     // Erstellen eines Baumes mit Werten aus einer Collection wie einer ArrayList, LinkedList etc.
-    public Suchbaum(Collection<E> c, E val) {
-        this.value = val;
-        for (E e : c) {
-            this.add(e);
-        }
+    public Suchbaum(Collection<E> c) {
+        
     }
 
     public Suchbaum<E> add(E e) {
-        if (this.value.compareTo(e) > 0) { // Wert geringerwertig als der Wert des aktuellen Knotens
-            if (this.left == null) {
-                this.left = new Suchbaum<E>(e);
-            } else {
-                this.left.add(e); // Falls bereits links ein Kind vorhanden ist, rufe die selbe Funktion in diesem Kindknoten auf
-            }
-        } else if (this.value.compareTo(e) <= 0) { // Wir erlauben hier durch das '<=' auch doppelte Werte
-            if (this.right == null) {
-                this.right = new Suchbaum<E>(e);
-            } else {
-                this.right.add(e);
-            }
-        }
-        return this;
+        // Implement me!
+        return null;
     }
 
     public E getMin() {
-        return (this.left == null) ? this.value : this.left.getMin();
+        // Implement me!
+        return null;
     }
 
     public E getMax() {
-        return (this.right == null) ? this.value : this.right.getMax();
+        // Implement me!
+        return null;
     }
 
     public boolean contains(E val) {
-        if (this.value == val) {
-            return true;
-        } else {
-            if (this.value.compareTo(val) > 0 && this.left != null) { // val ist kleiner und links existiert ein Kindknoten
-                return this.left.contains(val); // Funktion links aufrufen
-            } else if (this.value.compareTo(val) <= 0 && this.right != null) { // val ist größer und rechts existiert ein Kindknoten
-                return this.right.contains(val);
-            } else { // val ist kleiner und links ist kein Kindknoten mehr || val ist größer und rechts ist kein Kindknoten mehr
-                return false;
-            }
-        }
+        // Implement me!
+        return false;
+    }
+
+    @Override
+    public int compareTo(E o) {
+        return ((Comparable<E>) this.value).compareTo(o);
     }
 }
