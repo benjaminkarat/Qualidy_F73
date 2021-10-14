8c1f69ee55244ec8b118ef1adfa36e986dde9394
Benjamin Karatschinez <b.karatschinez@tu-bs.de>
2021-10-14 20:31:13 +0100

    Beautifix

--- Übung6/Suchbaum/Main.java
+++ Übung6/Suchbaum/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Suchbaum<Integer> sb = new Suchbaum<>();
+        Suchbaum<Integer> sb = new Suchbaum<>(10);
         //sb.add(1);
         //sb.contains(1);
         //sb.getMin();
